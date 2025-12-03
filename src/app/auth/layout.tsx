a1be232c143e505bd8e924fa7c64619d90bd1db3
Matheus Almeida <malmeidaarruda2@gmail.com>
2025-12-03 00:49:04 +0000

    coloque o formulario mais ao centro da pagina

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,6 @@
 
 import { FuelPumpIcon } from "@/components/icons";
+import Link from "next/link";
 
 export default function AuthLayout({
     children,
@@ -9,5 +10,12 @@ export default function AuthLayout({
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
             <div className="absolute top-8 flex items-center gap-2">
-                <FuelPumpIcon className="size-8 text-primary" />
-                <
+                 <Link href="/" className="flex items-center gap-2">
+                    <FuelPumpIcon className="size-8 text-primary" />
+                    <h1 className="text-xl font-semibold">TanqueCheio</h1>
+                </Link>
+            </div>
+            {children}
+        </div>
+    );
+}
