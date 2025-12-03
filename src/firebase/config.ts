2d25dadb2551ede068c22a5657586439c54c8351
Matheus Almeida <malmeidaarruda2@gmail.com>
2025-12-03 03:43:01 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "aplicativo-de-ve-culos",
-  "appId": "1:932906534526:web:10c85b195a6396452f1595",
-  "storageBucket": "aplicativo-de-ve-culos.appspot.com",
-  "apiKey": "AIzaSyDa-kC9gBPJ69xAp1-25f0aZ2F2o_hFj0",
-  "authDomain": "aplicativo-de-ve-culos.firebaseapp.com",
-  "messagingSenderId": "932906534526"
+  "projectId": "studio-3180919710-a8f8c",
+  "appId": "1:3683839058:web:97c879d69e050ec843c38b",
+  "apiKey": "AIzaSyCVRTqDpQERescTC10oqf39nZD5xgZ4upg",
+  "authDomain": "studio-3180919710-a8f8c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "3683839058"
 };
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,17 +2,34 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { Auth, getAuth } from 'firebase/auth';
-import { Firestore, getFirestore } from 'firebase/firestore'
-import { createContext, useContext } from 'react';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (!getApps().length) {
-    initializeApp(firebaseConfig);
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
+    }
+
+    return getSdks(firebaseApp);
   }
-  const firebaseApp = getApp();
-  return getSdks(firebaseApp);
+
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
@@ -23,44 +40,11 @@ export function getSdks(firebaseApp: FirebaseApp) {
   };
 }
 
-
-export interface FirebaseContextState {
-  firebaseApp: FirebaseApp | null;
-  auth: Auth | null;
-  firestore: Firestore | null;
-}
-
-export const FirebaseContext = createContext<FirebaseContextState | undefined>(undefined);
-
-export const useAuth = (): Auth => {
-    const context = useContext(FirebaseContext);
-    if (context === undefined) {
-        throw new Error('useAuth must be used within a FirebaseProvider');
-    }
-    if (!context.auth) {
-        throw new Error('Auth service not available. Check FirebaseProvider.');
-    }
-    return context.auth;
-};
-
-export const useFirestore = (): Firestore => {
-    const context = useContext(FirebaseContext);
-    if (context === undefined) {
-        throw new Error('useFirestore must be used within a FirebaseProvider');
-    }
-     if (!context.firestore) {
-        throw new Error('Firestore service not available. Check FirebaseProvider.');
-    }
-    return context.firestore;
-};
-
+export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
-export * from './auth/use-user';
 export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
-export * from './provider';
-export * from './hooks/use-memo-firebase';
