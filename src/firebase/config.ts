3b173b9570a51bcc47196071ee25424f2bde8a41
Matheus Almeida <malmeidaarruda2@gmail.com>
2025-12-03 04:03:32 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,12 @@
 
 
 export const firebaseConfig = {
-    apiKey: "__REPLACE_ME__",
-    authDomain: "__REPLACE_ME__",
-    projectId: "__REPLACE_ME__",
-    storageBucket: "__REPLACE_ME__",
-    messagingSenderId: "__REPLACE_ME__",
-    appId: "__REPLACE_ME__",
-    measurementId: "__REPLACE_ME__",
+    apiKey: "AIzaSyCVRTqDpQERescTC10oqf39nZD5xgZ4upg",
+    authDomain: "studio-3180919710-a8f8c.firebaseapp.com",
+    projectId: "studio-3180919710-a8f8c",
+    storageBucket: "studio-3180919710-a8f8c.appspot.com",
+    messagingSenderId: "3683839058",
+    appId: "1:3683839058:web:97c879d69e050ec843c38b",
+    measurementId: "G-85P0Z9Z48C",
 };
+
